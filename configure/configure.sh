383691f09e4a149ef873e92389abf43e470bb211
kefalak <91338144+kefalak@users.noreply.github.com>
2024-03-06 13:36:02 +0200

    dotnet repo set

--- configure/configure.sh
+++ configure/configure.sh
@@ -3,13 +3,13 @@
 declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 
 # Download Microsoft signing key and repository
-#wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 
 # Install Microsoft signing key and repository
-#sudo dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 
 # Clean up
-#rm packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
 # Update packages
-#sudo apt update
+sudo apt update

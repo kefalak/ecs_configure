b3756d352484dca71b923b679cc460dc0297d25d
kefalak <91338144+kefalak@users.noreply.github.com>
2024-03-15 21:10:59 +0200

    added nvm dirs

--- configure/04_node_install.sh
+++ configure/04_node_install.sh
@@ -2,5 +2,6 @@
 sudo apt install wget
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash
 source ~/.profile
-. ~/.profile
-~/nvm install --lts
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+nvm install --lts
